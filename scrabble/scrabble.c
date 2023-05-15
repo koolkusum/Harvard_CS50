15214cb1b1ba68029d96c0d09b62811568db20c4
Kusum Gandham <koolkusum10@gmail.com>
2023-05-15 19:58:04 -0400

    scrabble

---
+++ scrabble/scrabble.c
@@ -0,0 +1,60 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+//a starts a 0 and Z ends at 25
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    //need to do comparison
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    //calculating length
+    int length = strlen(word);
+    //Capital A to Capital Z ASCII 65-90
+    //lowercase a to lowercase z ASCII  97-22
+
+    // TODO: Compute and return score for string
+    //intializing score
+    int score = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+        else if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+    }
+    return score;
+}
